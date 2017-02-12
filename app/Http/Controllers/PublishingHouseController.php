c79649c69c3ae1b652ce5c64abd1855079fba828
Allan Gonçalves da Cruz <allangcruz@gmail.com>
2017-02-12 11:57:44 -0200

    add migration basics

---
+++ app/Http/Controllers/PublishingHouseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Library\Http\Controllers;
+
+use Library\Models\PublishingHouse;
+use Illuminate\Http\Request;
+
+class PublishingHouseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Library\Models\PublishingHouse  $publishingHouse
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PublishingHouse $publishingHouse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Library\Models\PublishingHouse  $publishingHouse
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PublishingHouse $publishingHouse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Library\Models\PublishingHouse  $publishingHouse
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PublishingHouse $publishingHouse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Library\Models\PublishingHouse  $publishingHouse
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PublishingHouse $publishingHouse)
+    {
+        //
+    }
+}
